1aa968e039dbb8f4f3309e7b9a38ec1749be06ec
James Talmage <james.talmage@jrtechnical.com>
2015-02-14 01:08:46 -0500

    TreeNode events and initialization

--- src/TreeNode.js
+++ src/TreeNode.js
@@ -1,31 +1,112 @@
 'use strict';
 
-function TreeNode(){
+function TreeNode(key, parent){
+  this._parent = parent || null;
   this._callbacks = null;
   this._children = {};
-  this._value = undefined;
-  this._pending_value = undefined;
+  this._changedChildren = [];
+  this._value = null;
+  this._pendingValue = null;
+  this._changed = false;
+  this.key = key;
+  this.initialized = false;
+  this._initializeNextFlush = false;
 }
 
 require('./EventEmitter')(TreeNode.prototype);
 
-TreeNode.prototype.setValue = function(value){
-  if(value === undefined) {
-    value = null;
-  }
-  this._pending_value = value;
-  return this._value !== this._pending_value;
-};
-
 TreeNode.prototype.flushChanges = function(){
-  if(this._value !== this._pending_value){
-    this._value = this._pending_value;
+  var changedChildren = this._changedChildren;
+  if(changedChildren.length){
+    this._changedChildren = [];
+    changedChildren.forEach(function(child){
+      child.flushChanges();
+    });
+  }
+  if(this._changed || this._initializeNextFlush){
+    this._changed = false;
+    this._value = this._pendingValue;
     this.emit('value');
   }
+  if(this._initializeNextFlush) {
+    this._initializeNextFlush = false;
+    this.initialized = true;
+  }
 };
 
-TreeNode.prototype.isInitialized = function(){
-  return this._value !== undefined;
+TreeNode.prototype.setValue = function(value){
+  this._initializeNextFlush = !this.initialized;
+  return (this._changed = this._setValue(value));
+};
+
+TreeNode.prototype._setValue = function(value){
+  if(typeof value === 'object' && value !==null){
+    var children = this._children;
+    var changed = false;
+    for(var i in children){
+      /* istanbul ignore else */
+      if(children.hasOwnProperty(i)){
+        changed = children[i].setValue(value.hasOwnProperty(i) ? value[i] : null) || changed;
+      }
+    }
+    for(var j in value){
+      if(value.hasOwnProperty(j) && !children.hasOwnProperty(j)){
+        var child = children[j] = new TreeNode(j, this);
+        changed = child.setValue(value[j]) || changed;
+      }
+    }
+    if(changed){
+      this._registerChange();
+    }
+    return changed;
+  }
+  else {
+    this._pendingValue = value;
+    if(value !== this._value){
+      this._registerChange();
+      return true;
+    }
+    return false;
+  }
+};
+
+TreeNode.prototype._registerChange = function(){
+  if(!this._changed){
+    this._changed = true;
+    var parent = this._parent;
+    if(parent){
+      parent._registerChangedChild(this);
+    }
+  }
+};
+
+TreeNode.prototype._registerChangedChild = function(child){
+  this._changedChildren.push(child);
+};
+
+TreeNode.prototype.child = function(path,create){
+  path = path.split('/');
+  var child = this;
+  var i,len;
+  if (create) {
+    for (i = 0, len = path.length; i < len; i++) {
+      child = child._getOrCreateChild(path[i]);
+    }
+  } else {
+    for (i = 0, len = path.length; child && i < len; i++) {
+      child = child._getChild(path[i]);
+    }
+  }
+  return child;
+};
+
+TreeNode.prototype._getChild = function(key){
+  return this._children[key] || null;
+};
+
+TreeNode.prototype._getOrCreateChild = function(key){
+  var children = this._children;
+  return children[key] || (children[key] = new TreeNode(key,this));
 };
 
 module.exports = TreeNode;
--- test/TreeNode-test.js
+++ test/TreeNode-test.js
@@ -1,4 +1,4 @@
-xdescribe('TreeNode',function(){
+describe('TreeNode',function(){
 
   // Assertion Framework
   var chai = require('chai');
@@ -47,13 +47,17 @@ xdescribe('TreeNode',function(){
         expect(node.setValue(1)).to.equal(true);
       });
 
-      it('null',function(){
-        expect(node.setValue(null)).to.equal(true);
-      });
-
       it('object',function(){
         expect(node.setValue({a:'a'})).to.equal(true);
       });
+
+      it('deep object',function(){
+        expect(node.setValue({a:{b:'c'}})).to.equal(true);
+      });
+    });
+
+    it('returns false the first time it is set with null',function(){
+      expect(node.setValue(null)).to.equal(false);
     });
 
     describe('returns true if value is changed',function(){
@@ -90,6 +94,12 @@ xdescribe('TreeNode',function(){
         node.flushChanges();
         expect(node.setValue({a:'b'})).to.equal(true);
       });
+
+      it('deep object',function(){
+        node.setValue({a:{b:'c'}});
+        node.flushChanges();
+        expect(node.setValue({a:{b:'d'}})).to.equal(true);
+      });
     });
 
     describe('returns false if value is unchanged ',function(){
@@ -117,68 +127,174 @@ xdescribe('TreeNode',function(){
         expect(node.setValue(null)).to.equal(false);
       });
 
-      it('key ordering',function(){
-        expect(Object.keys({a:'a',b:'b'}).sort()).to.eql(Object.keys({b:'a',a:'b'}).sort())
-      });
-
-      xit('deeply unequal object',function(){
+      it('deeply unequal object',function(){
         node.setValue({a:'a'});
         node.flushChanges();
         expect(node.setValue({a:'a'})).to.equal(false);
       });
+
+      it('deep object',function(){
+        node.setValue({a:{b:'c'}});
+        node.flushChanges();
+        expect(node.setValue({a:{b:'c'}})).to.equal(false);
+      });
+
+      it('called twice before flushed',function(){
+        node.setValue('a');
+        node.flushChanges();
+        expect(node.setValue('b')).to.equal(true);
+        expect(node.setValue('a')).to.equal(false);
+      });
+
+      it('called twice before flushed - deep object',function(){
+        node.setValue({a:{b:{c:'c'}}});
+        node.flushChanges();
+        expect(node.setValue({a:{b:{c:'d'}}})).to.equal(true);
+        expect(node.setValue({a:{b:{c:'c'}}})).to.equal(false);
+      });
+
+      it('called twice before flushed - deep object - with new properties',function(){
+        node.setValue({a:{b:{c:'c'}}});
+        node.flushChanges();
+        expect(node.setValue({a:{b:{d:'d'}}})).to.equal(true);
+        expect(node.setValue({a:{b:{c:'c'}}})).to.equal(false);
+      });
     });
   });
 
-  describe('#isInitialized() ',function(){
+  describe('#initialized ',function(){
     it('returns false before setValue is called',function(){
       node.flushChanges();
-      expect(node.isInitialized()).to.equal(false);
+      expect(node.initialized).to.equal(false);
     });
 
     describe('returns true once setValue is called with a ',function(){
       it('string',function(){
         node.setValue('foo');
         node.flushChanges();
-        expect(node.isInitialized()).to.equal(true);
+        expect(node.initialized).to.equal(true);
       });
 
       it('boolean',function(){
         node.setValue(false);
         node.flushChanges();
-        expect(node.isInitialized()).to.equal(true);
+        expect(node.initialized).to.equal(true);
       });
 
       it('number',function(){
         node.setValue(3);
         node.flushChanges();
-        expect(node.isInitialized()).to.equal(true);
+        expect(node.initialized).to.equal(true);
       });
 
       it('null',function(){
         node.setValue(null);
         node.flushChanges();
-        expect(node.isInitialized()).to.equal(true);
+        expect(node.initialized).to.equal(true);
+      });
+
+      it('object', function(){
+        node.setValue({a:'b'});
+        node.flushChanges();
+        expect(node.initialized).to.equal(true);
       });
     });
   });
 
   describe('#on() ',function(){
-    it('"value" events are called when value changes', function(){
-      node.on('value',spy1);
-      node.setValue(true);
-      expect(spy1).not.to.have.been.called;
-      node.flushChanges();
-      expect(spy1).to.have.been.called;
-    });
+    describe('"value" listeners ',function(){
+      it('are not called if setValue has never been called',function(){
+        node.on('value',spy1);
+        node.flushChanges();
+        expect(spy1).not.to.have.been.called;
+      });
 
-    it('"value" events are not called when setValue() does not change anything',function(){
-      node.setValue('foo');
-      node.on('value',spy1);
-      node.flushChanges();
-      spy1.reset();
-      node.setValue('foo');
-      node.flushChanges();
-      expect(spy1).not.to.have.been.called;
+      it('is called if setValue was initially set to null',function(){
+        node.on('value',spy1);
+        node.setValue(null);
+        node.flushChanges();
+        expect(spy1).to.have.been.called;
+      });
+
+      it('are called when value changes', function(){
+        node.on('value',spy1);
+        node.setValue(true);
+        expect(spy1).not.to.have.been.called;
+        node.flushChanges();
+        expect(spy1).to.have.been.called;
+      });
+
+      it('are not called when setValue() does not change anything',function(){
+        node.setValue('foo');
+        node.on('value',spy1);
+        node.flushChanges();
+        spy1.reset();
+        node.setValue('foo');
+        node.flushChanges();
+        expect(spy1.called).to.equal(false);
+      });
+
+      describe('on child nodes',function(){
+        it('are called when the child\'s value changes' ,function(){
+          node.setValue({a:'a'});
+          node.child('a').on('value',spy1);
+          node.flushChanges();
+          spy1.reset();
+          node.setValue({a:'b'});
+          node.flushChanges();
+          expect(spy1.called).to.equal(true);
+        });
+
+        it('are not called when the child\'s value stays the same' ,function(){
+          node.setValue({a:'a'});
+          node.child('a').on('value',spy1);
+          node.flushChanges();
+          spy1.reset();
+          node.setValue({a:'a'});
+          node.flushChanges();
+          expect(spy1.called).to.equal(false);
+        });
+      });
     });
   });
+
+  describe('#key ',function(){
+    it('contains the key passed to constructor',function(){
+      node = new TreeNode('a');
+      expect(node.key).to.equal('a');
+    });
+  });
+
+  describe('#child(path, [create]) ', function(){
+    it('will return null if no child exists', function(){
+      node.setValue({a:'a'});
+      expect(node.child('b')).to.equal(null);
+    });
+
+    it('will return the child if one exists', function(){
+      node.setValue({a:'a'});
+      expect(node.child('a')).to.be.an.instanceOf(TreeNode);
+      expect(node.child('a').key).to.equal('a');
+    });
+
+    it('will return a deep child', function(){
+      node.setValue({a:{b:'c'}});
+      expect(node.child('a/b')).to.be.an.instanceOf(TreeNode);
+      expect(node.child('a/b').key).to.equal('b');
+    });
+
+    it('will create a child even if one does not exist', function(){
+      node.setValue({a:'a'});
+      expect(node.child('b')).to.equal(null);
+      expect(node.child('b',true)).to.be.an.instanceOf(TreeNode);
+    });
+
+    it('returns the same node each time',function(){
+      node.setValue({a:'a'});
+      expect(node.child('a')).to.equal(node.child('a'));
+      expect(node.child('b',true)).to.equal(node.child('b')).and.to.not.equal(null);
+    });
+  });
+
+
 });
